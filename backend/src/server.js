3594a7ca8fa6f752d9dcebe821871876c549d8f9
Shaikira <your@email.com>
2026-05-03 12:28:30 +0800

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,13 @@ import {connectDB} from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path'
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 connectDB().then(()=>{
   app.listen(port, () => {
@@ -18,17 +20,25 @@ connectDB().then(()=>{
 
 //middleware
 app.use(express.json());
-app.use(cors({
+
+if(process.env.process === "development"){
+  app.use(cors({
   origin: 'http://localhost:5173', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true
 }));
 app.use(rateLimiter);
+}
+
 
 
 app.use('/api/notes', router)
 
+if(process.env.PROCESS === "production"){
+  app.use(express.static(path.join(__dirname, '../frontend/NoteTaker/dist')))
 
-
-
+  app.get("*", (req,res)=>{
+  res.sendFile(path.join(__dirname, '../frontend/NoteTaker', "dist", "index.html"))
+});
+}
